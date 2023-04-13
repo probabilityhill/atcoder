09fdca6aa26639f9122aa916b44d1eaaf708aa6f
okadanao <74280232+probabilityhill@users.noreply.github.com>
2023-04-13 22:52:19 +0900

    Create Main.java

---
+++ lesson/ITP1/5_C/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int h;
+        int w;
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            h = sc.nextInt();
+            w = sc.nextInt();
+            if(h + w == 0) {
+                break;
+            }
+            for(int i = 0; i < h; i++) {
+                for(int j = 0; j < w; j++) {
+                    if((i + j) % 2 == 0) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(".");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
